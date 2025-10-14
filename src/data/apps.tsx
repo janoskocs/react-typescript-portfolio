ccff447cf83564aec60ced12ad70fc6927e856d1
Janos Kocs <janos.kocs@outlook.com>
2025-10-14 20:14:33 +0100

    feat: add list of apps and links

--- src/data/apps.tsx
+++ src/data/apps.tsx
@@ -17,16 +17,9 @@ type AppType = {
   showOn: {
     desktop: boolean;
     startMenu: boolean;
-    projects: boolean;
   };
   categories: string[];
   component: ComponentKey | null;
-  repositoryLinks: {
-    clientLink: string | null;
-    serverLink: string | null;
-    genericLink: string | null;
-    liveLink: string | null;
-  };
   featured: boolean;
 };
 
@@ -48,46 +41,181 @@ const apps: AppType[] = [
     showOn: {
       desktop: true,
       startMenu: true,
-      projects: false,
     },
     categories: ["personal", "about"],
     component: "AboutMePage",
-    repositoryLinks: {
-      clientLink: null,
-      serverLink: null,
-      genericLink: null,
-      liveLink: null,
-    },
-    featured: true,
+    featured: false,
   },
   {
     id: "contact-me",
-    name: "contact Me",
+    name: "Contact Me",
     link: {
       type: "internal",
       url: "/contact-me",
     },
     title: "Open contact me section",
     description:
-      "Janos is a full stack developer specialising in React and Node.js.",
+      "Get in touch with me to discuss tech, swimming, and more code!",
     icon: {
       src: "contact-me.png",
-      alt: "Profile icon",
+      alt: "Envelope",
     },
     showOn: {
       desktop: true,
       startMenu: true,
-      projects: false,
     },
-    categories: ["personal", "about"],
+    categories: ["personal", "contact"],
     component: "ContactMePage",
-    repositoryLinks: {
-      clientLink: null,
-      serverLink: null,
-      genericLink: null,
-      liveLink: null,
-    },
     featured: true,
+  },
+  {
+    id: "janos-ai",
+    name: "JanosAI",
+    link: {
+      type: "internal",
+      url: "/janos-ai",
+    },
+    title: "Chat with my AI-self and learn more about my experience.",
+    description:
+      "JanosAI is a chat bot using Gemini API to share my experience with users.",
+    icon: {
+      src: "janos-ai.png",
+      alt: "Robot",
+    },
+    showOn: {
+      desktop: true,
+      startMenu: true,
+    },
+    categories: ["AI", "chat"],
+    component: "JanosAIPage",
+    featured: true,
+  },
+  {
+    id: "my-cv",
+    name: "My CV",
+    link: {
+      type: "internal",
+      url: "/my-cv",
+    },
+    title: "Check out my CV to learn more about my experience.",
+    description:
+      "Checkout and download my CV to learn more about my experience.",
+    icon: {
+      src: "my-cv.png",
+      alt: "Document",
+    },
+    showOn: {
+      desktop: true,
+      startMenu: true,
+    },
+    categories: ["CV", "career"],
+    component: "MyCVPage",
+    featured: true,
+  },
+  {
+    id: "my-projects",
+    name: "My Projects",
+    link: {
+      type: "internal",
+      url: "/my-projects",
+    },
+    title: "Check out my projects to learn more about my experience.",
+    description: "See a collection of my projects.",
+    icon: {
+      src: "my-projects.png",
+      alt: "Folder with document",
+    },
+    showOn: {
+      desktop: true,
+      startMenu: true,
+    },
+    categories: ["projects"],
+    component: "MyProjectsPage",
+    featured: true,
+  },
+  {
+    id: "linkedin",
+    name: "LinkedIn",
+    link: {
+      type: "external",
+      url: "https://www.linkedin.com/in/janoskocs/",
+    },
+    title: "Let's connect on LinkedIn!",
+    description: "Learn more about my experience.",
+    icon: {
+      src: "linkedin.png",
+      alt: "LinkedIn",
+    },
+    showOn: {
+      desktop: true,
+      startMenu: true,
+    },
+    categories: ["linkedin", "social"],
+    component: null,
+    featured: false,
+  },
+  {
+    id: "github",
+    name: "GitHub",
+    link: {
+      type: "external",
+      url: "https://github.com/janoskocs",
+    },
+    title: "Let's connect on GitHub!",
+    description: "Check out my work on GitHub..",
+    icon: {
+      src: "github.png",
+      alt: "GitHub",
+    },
+    showOn: {
+      desktop: true,
+      startMenu: true,
+    },
+    categories: ["github", "social"],
+    component: null,
+    featured: false,
+  },
+  {
+    id: "my-blog",
+    name: "My Blog",
+    link: {
+      type: "external",
+      url: "https://dev.to/janoskocs",
+    },
+    title: "Read my blog!",
+    description: "Read my blog where I share cool tech tutorials.",
+    icon: {
+      src: "my-blog.png",
+      alt: "My Blog",
+    },
+    showOn: {
+      desktop: true,
+      startMenu: true,
+    },
+    categories: ["blog", "social"],
+    component: null,
+    featured: false,
+  },
+  {
+    id: "youtube",
+    name: "My YouTube Channel",
+    link: {
+      type: "external",
+      url: "https://www.youtube.com/@janoscodes",
+    },
+    title: "Subscribe to my YouTube channel!",
+    description: "I share tech easy and quick tech tutorials.",
+    icon: {
+      src: "youtube.png",
+      alt: "My YouTube Channel",
+    },
+    showOn: {
+      desktop: true,
+      startMenu: true,
+    },
+    categories: ["youtube", "social"],
+    component: null,
+    featured: false,
   },
 ];
 
--- src/routes/lazyImports/lazyImports.public.tsx
+++ src/routes/lazyImports/lazyImports.public.tsx
@@ -7,12 +7,22 @@ const AboutMePage: React.LazyExoticComponent<React.ComponentType<{}>> =
   lazyImport("../pages/about-me/about-me");
 const ContactMePage: React.LazyExoticComponent<React.ComponentType<{}>> =
   lazyImport("../pages/contact-me/contact-me");
+const JanosAIPage: React.LazyExoticComponent<React.ComponentType<{}>> =
+  lazyImport("../pages/janos-ai/janos-ai");
+const MyCVPage: React.LazyExoticComponent<React.ComponentType<{}>> = lazyImport(
+  "../pages/my-cv/my-cv"
+);
+const MyProjectsPage: React.LazyExoticComponent<React.ComponentType<{}>> =
+  lazyImport("../pages/my-projects/my-projects");
 
 // export { AboutMePage, ContactMePage };
 
 export const componentMap = {
   AboutMePage,
   ContactMePage,
+  JanosAIPage,
+  MyCVPage,
+  MyProjectsPage,
 };
 
 export type ComponentKey = keyof typeof componentMap;
