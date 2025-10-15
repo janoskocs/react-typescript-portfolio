c5b441aaedea5e55349eac06dfeb4928461c2b46
Janos Kocs <janos.kocs@outlook.com>
2025-10-15 20:13:22 +0100

    chore: install tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import DesktopIcons from "./features/desktop-icons/desktop-icons";
 import TaskbarFooter from "./features/taskbar-footer/taskbar-footer";
 import { AppRoutes } from "./routes/AppRoutes";
+import "./App.css";
 
 const App = () => {
   return (
