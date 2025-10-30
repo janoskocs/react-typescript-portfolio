802208a661f244062047b58e679b75e2a969f8fc
Janos Kocs <janos.kocs@outlook.com>
2025-10-30 20:50:20 +0000

    style: update recaptcha styling

--- src/context/providers.tsx
+++ src/context/providers.tsx
@@ -5,7 +5,8 @@ type ProvidersProps = {
 };
 
 const Providers = ({ children }: ProvidersProps) => {
-  const siteKey = import.meta.env.PUBLIC_RECAPTCHA_SITE_KEY;
+  const siteKey = import.meta.env.VITE_PUBLIC_RECAPTCHA_SITE_KEY;
+  console.log("ReCAPTCHA Site Key:", siteKey);
   return (
     <GoogleReCaptchaProvider
       reCaptchaKey={siteKey}
